4861755f6ebd785faf3476ca9a297fb1101b1bbc
Lucas Barros Bonine <lucasbonine@live.com>
2024-11-05 07:34:22 -0300

    Finalizando retornos API

--- src/main/java/med/voll/api/rest/MedicoRest.java
+++ src/main/java/med/voll/api/rest/MedicoRest.java
@@ -1,5 +1,8 @@
 package med.voll.api.rest;
 
+import java.net.URI;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.dtos.AtualizacaoMedicoDTO;
@@ -21,6 +25,7 @@ import med.voll.api.dtos.DadosDetalhamentoMedicoDTO;
 import med.voll.api.dtos.DadosListagemMedico;
 import med.voll.api.exceptions.ApplicationException;
 import med.voll.api.service.MedicoService;
+import med.voll.api.vo.MedicoVO;
 
 @RestController
 @RequestMapping("/medicos")
@@ -29,21 +34,28 @@ public class MedicoRest {
 	@Autowired
 	private MedicoService medicoService;
 	
+	/*
+	 * O post precisa devolver um código 201 (Created), 
+	 * o Body com o objeto criado e 
+	 * uma URI com o endereço do recurso criado no header
+	 */
 	@PostMapping
-	public void cadastrar(@RequestBody @Valid CadastroMedicoDTO dados) {
-		medicoService.cadastrar(dados);
+	public ResponseEntity cadastrar(@RequestBody @Valid CadastroMedicoDTO dados, UriComponentsBuilder uriBuilder) {
+		MedicoVO medicoCriado = medicoService.cadastrar(dados);
+		URI uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medicoCriado.getId()).toUri();
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedicoDTO(medicoCriado)); //201
 	}
 	
 	@GetMapping
 	public ResponseEntity<Page<DadosListagemMedico>> listar(Pageable paginacao){
-		return ResponseEntity.ok(medicoService.listar(paginacao));
+		return ResponseEntity.ok(medicoService.listar(paginacao)); //200
 	}
 	
 	@PutMapping
 	public ResponseEntity<DadosDetalhamentoMedicoDTO> atualizar(@RequestBody @Valid AtualizacaoMedicoDTO dados) {		
 		try {
 			DadosDetalhamentoMedicoDTO retornoAtualizacao = medicoService.atualizar(dados);
-			return ResponseEntity.ok(retornoAtualizacao);
+			return ResponseEntity.ok(retornoAtualizacao); //200
 		} catch (ApplicationException e) {
 			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
@@ -53,11 +65,22 @@ public class MedicoRest {
 
 	}
 	
-	@SuppressWarnings("rawtypes")
 	@DeleteMapping("/{id}")
 	public ResponseEntity excluir(@PathVariable Long id) {
 		medicoService.excluir(id);
 		return ResponseEntity.noContent().build(); //204
+	}
+	
+
+	@GetMapping("/{id}")
+	public ResponseEntity detalhar(@PathVariable Long id) throws ApplicationException {		
+		Optional<MedicoVO> medicoBuscado = medicoService.buscarMedicoPorId(id);
+
+		if(medicoBuscado.isPresent()) {
+			return ResponseEntity.ok(new DadosDetalhamentoMedicoDTO(medicoBuscado.get()));
+		}else {
+			throw new ApplicationException("Medico de id ".concat(String.valueOf(id)).concat(" não localizado."));
+		}
 	}
 }
 
