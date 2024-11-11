63d9de4531628e036eb345dee0afa195f1cee3e9
Lucas Barros Bonine <lucasbonine@live.com>
2024-11-11 08:57:27 -0300

    Criação entidade Usuario

---
+++ src/main/java/med/voll/api/vo/UsuarioVO.java
@@ -0,0 +1,44 @@
+package med.voll.api.vo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioVO {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String senha;
+	
+	public UsuarioVO() {
+
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
